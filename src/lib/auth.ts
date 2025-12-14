0ed30c7ad9a8c4c0c727143a93ebd9130176847b
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2025-12-14 15:56:19 +0700

    fix_linting_error

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@ import type { NextAuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 import bcrypt from "bcrypt"
 import prisma from "./prisma"
+import { Role } from "@/generated/prisma/enums"
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -58,7 +59,7 @@ export const authOptions: NextAuthOptions = {
       if (session.user) {
         session.user.id = token.id as string
         session.user.name = token.name as string
-        session.user.role = token.role as string
+        session.user.role = token.role as Role
       }
       return session
     },
