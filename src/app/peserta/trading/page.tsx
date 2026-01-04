0f87fa6f13f94b7aba1fd7de46fbcd81a24720cc
felix <ffelixrichardo@gmail.com>
2026-01-04 14:35:02 +0700

    match UI design on player with CardPanel

--- src/app/peserta/trading/page.tsx
+++ src/app/peserta/trading/page.tsx
@@ -11,12 +11,8 @@ import { redirect } from "next/navigation";
 export default async function Page() {
   const session = await getServerSession(authOptions);
   
-  if (!session?.user?.id) {
-    redirect("/login");
-  }
-
   // 1. Get User Data
-  const userRes = await getUserTradingById(session.user.id);
+  const userRes = await getUserTradingById(session!.user.id);
   
   if (!userRes.success || !userRes.data) {
      return <div className="text-white text-center mt-20">Error loading user data: {userRes.error}</div>;
@@ -28,10 +24,14 @@ export default async function Page() {
   const rawItemAmount = tradingData.rawUserAmounts.reduce((sum, item) => sum + Number(item.amount), 0);
   const craftItemAmount = tradingData.craftUserAmounts.reduce((sum, item) => sum + Number(item.amount), 0);
 
+  const formatBigInt = (val: bigint | number) => {
+    return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  };
+
   const stats = {
-    usd: Number(tradingData.usd).toLocaleString('en-US'),
-    idr: Number(tradingData.idr).toLocaleString('id-ID'),
-    eternites: Number(tradingData.eternites).toLocaleString('id-ID'),
+    usd: formatBigInt(tradingData.usd),
+    idr: formatBigInt(tradingData.idr),
+    eternites: formatBigInt(tradingData.eternites),
     mapAmount: Number(tradingData.map || 0),
     rawItemAmount,
     craftItemAmount
@@ -59,7 +59,7 @@ export default async function Page() {
     <div className="overflow-hidden min-h-screen relative">
       <BackgroundAssetsDesktop />
       <BackgroundAssetsMobile />
-      <div className="absolute bg-gradient-to-b from-[7%] from-[#AE00DE]/0 to-[#23328C] w-full h-full top-0 left-0"></div>
+      <div className="absolute bg-gradient-to-b from-transparent via-[#AE00DE]/0 to-[#23328C] w-full h-full top-0 left-0"></div>
       
       <div className="relative z-10 flex flex-col items-center justify-start min-h-screen py-10 gap-8">
         <Image
