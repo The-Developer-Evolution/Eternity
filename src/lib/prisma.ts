10300031cf67f50ec3b867ae3d42ba20961795f7
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2026-01-16 17:29:07 +0700

    try prisma accelerate

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,23 +1,23 @@
-import { PrismaClient } from "@/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "@/generated/prisma/client";
+// import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaPg({ 
-  connectionString: process.env.DATABASE_URL 
-});
-const prisma = new PrismaClient({adapter});
-
-export default prisma;
-
-// import { PrismaClient } from "@prisma/client/scripts/default-index.js";
-// import { withAccelerate } from "@prisma/extension-accelerate";
-
-// const globalForPrisma = global as unknown as {
-//   prisma: PrismaClient;
-// };
-
-// const prisma =
-//   globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
-
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// const adapter = new PrismaPg({ 
+//   connectionString: process.env.DATABASE_URL 
+// });
+// const prisma = new PrismaClient({adapter});
 
 // export default prisma;
+
+import { PrismaClient } from "@prisma/client/scripts/default-index.js";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+const prisma =
+  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
