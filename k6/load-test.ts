e8f0cc524f5843fa7e187c0f0521c4c1c4643e8d
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2026-01-17 03:59:09 +0700

    optimize status

--- k6/load-test.ts
+++ k6/load-test.ts
@@ -21,7 +21,7 @@ export default function loadTest() {
   const params = {
     headers: {
       'Content-Type': 'application/json',
-      'Cookie': 'eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..Xfg_TWcjOcvUsJn5.cpkO8NZoQoPYPOmi-sGzK_woEvCXidkaF8Qbtkj5I-cFk1E5gSPbMQJE0luBakciFz2rdg-JKew6WT45NxEcE5zvZMJsaUR_MY__UqyJG-Xr6Uekmo8-GV_0f9AnYAms_AF4oPbwRnWDrLKNbiE2_FjJmfxWHYt17kjYa4TTTKXcmnVyVe8DkazSNrIyCPZu_033DnQuxv0ul1_DINToz5hF7azDLRG-aBpRUHwmh9mD.N_H6V-id2PixnLso9QDsSw'
+      'Cookie': 'eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..hUqNOYN30ass9pBS.fCQ6GOpGWs0WjAS3decwKFK_xbep3EGg-2_POH4yF8aTf8SjbV5EnbQPXFapFL6yVRnpyIQIzH8zxK5vg6vUoYtjBifS2h0DlOeOKcgAKBYofGH_AbMOmnPuGjOdfNiAdgjwg-fQzbhAapDcRo7urCc1yS-3Nzq2nSS2c_nwlEe--RCubheLpDCZ6i45R_AZGH7dp-0PmBE5tIrBMOLH63nqdsALjhp_1Ubnp6twKUDw.o5Z1IBKC0Szc0erlChg4rQ'
     },
   };
 
